46fdd85ea43a647df162d18357b8cc15a12ed5be
marcusbesjes <marcusbesjes@gmail.com>
2015-11-14 19:51:43 +0100

    start stub

---
+++ README.md
@@ -0,0 +1,2 @@
+# Facebook
+Vigorous facebook!
---
+++ test/browser/nativePlugin.js
@@ -0,0 +1,19 @@
+'use strict'
+var devBridge = require('vigour-wrapper/dev/bridgemock')
+
+describe('Testing Facebook Native Plugin', function () {
+
+  it('should be able to create a plugin instance', function (done) {
+
+  })
+
+  describe('Facebook API', function () {
+    it('should be able to log in', function () {
+
+    })
+
+    it('should be able to share in', function () {
+
+    })
+  })
+})
