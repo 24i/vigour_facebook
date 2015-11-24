281b56e5318dfcde034868f89043519bbc03f831
marcusbesjes <marcusbesjes@gmail.com>
2015-11-24 22:57:35 +0100

    testings

--- test/tests.js
+++ test/tests.js
@@ -4,8 +4,9 @@ module.exports = function runTests () {
   it('should get initial login in state on init', (done) => {
     facebook = window.vigour_facebook
     facebook.init()
-    facebook.connectionStatus.once(() => {
+    facebook.connectionStatus.once(function onConnectionStatus() {
       alert('connectionStatus: ' + facebook.connectionStatus.val)
+      facebook.connectionStatus.off(onConnectionStatus)
       done()
     })
   })
@@ -25,45 +26,56 @@ module.exports = function runTests () {
       let cnt = 0
       facebook.login.val = true
       facebook.login.once(function onLogin() {
+        facebook.login.off(onLogin)
         console.log('login once')
         expect(facebook.login.val).to.equal('success')
         if(++cnt === 2) {
+          console.error('call done')
           done()
         }
       })
-      facebook.token.once(function() {
+      facebook.token.once(function onToken() {
+        facebook.token.off(onToken)
         console.log('token once!', facebook.token.val)
         expect(facebook.token.val).to.be.ok
         if(++cnt === 2) {
+          console.error('call done')
           done()
         }
-        // facebook.token.off(onToken)
       })
     }
   })
 
-  xit('should be able to share', (done) => {
+  it('should be able to share', (done) => {
     facebook.share.val = 'http://www.google.com'
-    facebook.share.once(() => {
+    facebook.share.once( function onShared() {
+      facebook.share.off(onShared)
       console.log('shared!', facebook.share.val)
       expect(facebook.share.val).to.equal('success')
       done()
     })
   })
 
-  xit('should be able to log out', (done) => {
+  it('should be able to log out', (done) => {
+    console.error('logging out!')
     facebook.login.val = false
     expect(facebook.token.val).to.be.false
-    facebook.loading.once(() => {
-      console.log('loading change after logout', facebook.loading.val)
+    if(facebook.loading.val) {
+      facebook.loading.once(function onLoading() {
+        facebook.loading.off(onLoading)
+        console.log('loading change after logout', facebook.loading.val)
+        done()
+      })
+    } else {
       done()
-    })
-    
+    }    
   })
 
-  xit('should be able to share from non logged in state', () => {
+  it('should be able to share from non logged in state', () => {
+    console.error('sharing from logged out state out!')
     facebook.share.val = 'http://www.vigour.io'
-    facebook.share.once(() => {
+    facebook.share.once( function onShare() {
+      facebook.share.off(onShare)
       console.log('shared!', facebook.share.val)
       expect(facebook.share.val).to.equal('success')
       expect(facebook.token.val).to.be.ok
