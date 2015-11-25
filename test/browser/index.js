13a91be25508d611821d2062bcdafca4fb81dadc
Shawn Inder <shawn@vigour.io>
2015-11-25 17:32:07 -0500

    lint

--- test/browser/index.js
+++ test/browser/index.js
@@ -3,32 +3,31 @@
 var ua = require('vigour-ua')
 var agent = ua(navigator.userAgent)
 if (agent.platform === 'android' || agent.platform === 'ios') {
-	require('gaston-tester')
+  require('gaston-tester')
 }
 
 var runTests = require('../tests')
 
 describe('Facebook Mock', function () {
-	describe('shared + mock JS methods', function() {
-		it('should require facebook API', function () {
-			var facebook = require('../../lib/mock')
-			window.vigour_facebook = facebook
-		})
-		describe('feature tests', function(){
-	    runTests()
-	  })
-	})
-	describe('shared + wrapped + mock native methods', function () {
-		it('should mutate bridge to insert mock native plugin', function () {
-			require('../mockNative')
-		})
-		it('should require facebook API', function () {
-			var facebook = require('../../lib/wrapped')
-			window.vigour_facebook = facebook
-		})
-		describe('feature tests', function () {
-	    runTests()
-	  })
-	})
-
+  describe('shared + mock JS methods', function () {
+    it('should require facebook API', function () {
+      var facebook = require('../../lib/mock')
+      window.vigour_facebook = facebook
+    })
+    describe('feature tests', function () {
+      runTests()
+    })
+  })
+  describe('shared + wrapped + mock native methods', function () {
+    it('should mutate bridge to insert mock native plugin', function () {
+      require('../mockNative')
+    })
+    it('should require facebook API', function () {
+      var facebook = require('../../lib/wrapped')
+      window.vigour_facebook = facebook
+    })
+    describe('feature tests', function () {
+      runTests()
+    })
+  })
 })
