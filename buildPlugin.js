0ba64ba73ef282537dfca526cd0aed17ea5352a4
Shawn Inder <shawn@vigour.io>
2016-02-06 18:44:07 +0100

    different build process per platform

--- buildPlugin.js
+++ buildPlugin.js
@@ -8,13 +8,15 @@ exports.start = function () {
   log.info('- configuring facebook -')
   var fbAppId = this.opts.vigour.facebook.appId
   log.info('App ID', fbAppId)
-  if (fbAppId) {
+  if (this.platform === 'android') {
     if (!this.strings) {
       this.strings = {}
     }
     if (!this.strings['facebook_app_id']) {
       this.strings['facebook_app_id'] = fbAppId
     }
+  } else if (this.platform === 'ios') {
+    // TODO
   }
   log.info('-----')
 }
