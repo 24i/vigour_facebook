b49313755f869c1408a122779e8e8680a4f65017
Shawn Inder <shawn@vigour.io>
2016-02-12 23:43:38 +0100

    Log error but succeed if not all facebook info is provided

--- buildPlugin.js
+++ buildPlugin.js
@@ -5,18 +5,22 @@ var log = require('npmlog')
 module.exports = exports = {}
 
 exports.start = function () {
-  log.info('- configuring facebook -')
-  var fbAppId = this.opts.vigour.facebook.appId
-  log.info('App ID', fbAppId)
-  if (this.platform === 'android') {
-    if (!this.strings) {
-      this.strings = {}
+  if (this.opts.vigour && this.opts.vigour.facebook && this.opts.vigour.facebook.appId) {
+    log.info('- configuring facebook -')
+
+    var fbAppId = this.opts.vigour.facebook.appId
+    log.info('App ID', fbAppId)
+    if (this.platform === 'android') {
+      if (!this.strings) {
+        this.strings = {}
+      }
+      if (!this.strings['facebook_app_id']) {
+        this.strings['facebook_app_id'] = fbAppId
+      }
+    } else if (this.platform === 'ios') {
+      // TODO
     }
-    if (!this.strings['facebook_app_id']) {
-      this.strings['facebook_app_id'] = fbAppId
-    }
-  } else if (this.platform === 'ios') {
-    // TODO
+  } else {
+    log.error('No `vigour.facebook.appId`', "can't configure facebook!")
   }
-  log.info('-----')
 }
